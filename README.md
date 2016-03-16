a93621837c750d2e45235e500b50811fa3df04fb
cable1985 <scable@unca.edu>
2016-03-16 10:59:19 -0400

    installed module

--- README.md
+++
@@ -1,1 +0,0 @@
-# WebTechProject1
---
+++ Twitter/tweetReader.js
@@ -0,0 +1,19 @@
+(function() {
+	"use strict";
+	
+	var Twitter = require("twitter"),
+			credentials = require("./credentials.json"),
+			client;
+
+	client = new Twitter(credentials);
+
+	client.stream(
+		"statuses/filter",
+		{"track": ["Cruz", "Ted Cruz", "Trump", "Donald Trump", "Sanders", "Bernie Sanders", "Clinton", "Hilary Clinton"]},
+		function(stream) {
+			stream.on("data", function(tweet) {
+				console.log(tweet.text);
+			});
+	});
+	
+}());
